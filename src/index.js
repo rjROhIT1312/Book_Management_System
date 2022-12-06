b79b0d25225c833027153f6cf8712b84f63f17d8
rjROhIT1312 <joshirohit44@gmail.com>
2022-12-06 17:22:54 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const mongoose=require('mongoose')
 const route=require('./route/route')
 const app=express()
 
+const multer= require("multer");
+const { AppConfig } = require('aws-sdk');
+
 app.use(express.json())
+
+app.use( multer().any())
 
 mongoose.connect("mongodb+srv://sarwjeet424:96568437528p@cluster0.8tsocgw.mongodb.net/group37Database",{useNewUrlParser:true})
 .then(()=> console.log("MongoDB connected"))
--- src/route/route.js
+++ src/route/route.js
@@ -1,4 +1,5 @@
 const express=require('express') 
+const aws= require("aws-sdk")
 const { createBook, getBooks, getBookReviewData ,updateBook ,deleteBook } = require('../controllers/bookController')
 const router= express.Router()
 const {authentication,authorisation} =require('../middleware/auth')
@@ -26,8 +27,64 @@ router.put('/books/:bookId/review/:reviewId',reviewUpdate)
 
 router.delete('/books/:bookId/review/:reviewId',reviewDeleteById)
 
-router.all("/*",function (req,res){
-    return res.status(404).send({status:false,message:"Page not found"})
+
+
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+    secretAccessKey: "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",
+    region: "ap-south-1"
 })
 
+let uploadFile= async ( file) =>{
+   return new Promise( function(resolve, reject) {
+    // this function will upload file to aws and return the link
+    let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+
+    var uploadParams= {
+        ACL: "public-read",
+        Bucket: "classroom-training-bucket",  //HERE
+        Key: "abc/" + file.originalname, //HERE 
+        Body: file.buffer
+    }
+
+
+    s3.upload( uploadParams, function (err, data ){
+        if(err) {
+            return reject({"error": err})
+        }
+        console.log(data)
+        console.log("file uploaded succesfully")
+        return resolve(data.Location)
+    })
+
+    // let data= await s3.upload( uploadParams)
+    // if( data) return data.Location
+    // else return "there is an error"
+
+   })
+}
+
+router.post("/write-file-aws", async function(req, res){
+
+    try{
+        let files= req.files
+        if(files && files.length>0){
+            //upload to s3 and get the uploaded link
+            // res.send the link back to frontend/postman
+            let uploadedFileURL= await uploadFile( files[0] )
+            res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
+        }
+        else{
+            res.status(400).send({ msg: "No file found" })
+        }
+        
+    }
+    catch(err){
+        res.status(500).send({msg: err})
+    }
+    
+})
+
+router.all("/*",function (req,res){
+    return res.status(404).send({status:false,message:"Page not found"})})
 module.exports=router
